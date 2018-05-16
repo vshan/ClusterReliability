e5508d65dee92f38b26ba34b4f2f429a112befbb
Arram Saichand <t-arsaic@microsoft.com>
2018-05-16 20:36:14 +0530

    Partition Mapping Done

---
+++ RestoreService/PartitionWrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Fabric.Health;
+using System.Fabric.Query;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoreService
+{
+    [DataContract]
+    class PartitionWrapper
+    {
+        [DataMember]
+        public Guid partitionId { get; set; }
+
+        [DataMember]
+        public string lastBackupAvailable{ get; set; }
+
+        [DataMember]
+        public string lastBackupRestored { get; set; }
+
+        public ServiceKind ServiceKind { get; set; }
+
+        public HealthState HealthState { get; set; }
+
+        public ServicePartitionInformation PartitionInformation { get;  set; }
+
+        public ServicePartitionStatus PartitionStatus { get;  set; }
+
+        public PartitionWrapper(Partition partition)
+        {
+            this.partitionId = partition.PartitionInformation.Id;
+            this.PartitionInformation = partition.PartitionInformation;
+            this.ServiceKind = partition.ServiceKind;
+            this.HealthState = partition.HealthState;
+            this.PartitionStatus = partition.PartitionStatus;
+        }
+    }
+}
--- RestoreService/RestoreService.cs
+++ RestoreService/RestoreService.cs
@@ -42,7 +42,7 @@ namespace RestoreService
             // TODO: Replace the following sample code with your own logic 
             //       or remove this RunAsync override if it's not needed in your service.
 
-            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+            //var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
             /*
             while (true)
             {
@@ -63,24 +63,121 @@ namespace RestoreService
                 }
                 await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
             }*/
-            GetApplicationsDeployedInCluster("localhost:19000");
+            await GetApplicationsDeployedInCluster("localhost:19000");
         }
 
-        public async  void GetApplicationsDeployedInCluster(String clusterConnectionString)
+        public async  Task GetApplicationsDeployedInCluster(String clusterConnectionString)
         {
             FabricClient fabricClient = new FabricClient(clusterConnectionString);
             FabricClient.QueryClient queryClient = fabricClient.QueryManager;
             System.Fabric.Query.ApplicationList appsList = await queryClient.GetApplicationListAsync();
             foreach(System.Fabric.Query.Application application in appsList)
             {
-                if (application != null)
-                {
-                    ServiceEventSource.Current.ServiceMessage(this.Context, "Application is "+ application.ApplicationName);
-                }
-                else
-                    Console.WriteLine("No applications");
+                ServiceEventSource.Current.ServiceMessage(this.Context, "Application is "+ application.ApplicationName);
+                await GetPartitionsOfApplication(application.ApplicationName, clusterConnectionString);
             }
-            Console.ReadKey();
+        }
+
+        public async Task GetPartitionsOfApplication(Uri applicationName, string clusterConnectionString)
+        {
+            FabricClient fabricClient = new FabricClient(clusterConnectionString);
+            IReliableDictionary<Guid, PartitionWrapper> myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<Guid, PartitionWrapper>>("myDictionary");
+            System.Fabric.Query.ServiceList services = await fabricClient.QueryManager.GetServiceListAsync(applicationName);
+            foreach(System.Fabric.Query.Service service in services)
+            {
+                System.Fabric.Query.ServicePartitionList partitions = await fabricClient.QueryManager.GetPartitionListAsync(service.ServiceName);
+                await MapPartitions(partitions, partitions, myDictionary);
+                using(var tx = this.StateManager.CreateTransaction())
+                {
+                    var result = await myDictionary.GetCountAsync(tx);
+                    ServiceEventSource.Current.ServiceMessage(this.Context, "The number of items in dictionary are : {0}", result);
+                    await tx.CommitAsync();
+                }
+            }
+        }
+
+        public async Task MapPartitions(System.Fabric.Query.ServicePartitionList partitionsInPrimary, System.Fabric.Query.ServicePartitionList partitionsInSecondary, IReliableDictionary<Guid, PartitionWrapper> myDictionary)
+        {
+            if (partitionsInPrimary != null)
+            {
+                ServicePartitionKind partitionKind = partitionsInPrimary[0].PartitionInformation.Kind;
+                if (partitionKind.Equals(ServicePartitionKind.Int64Range))
+                {
+                    await MapInt64Partitions(partitionsInPrimary, partitionsInSecondary, myDictionary);
+                }
+                else if (partitionKind.Equals(ServicePartitionKind.Named))
+                {
+                    await MapNamedPartitions(partitionsInPrimary, partitionsInSecondary, myDictionary);
+                }
+                else if (partitionKind.Equals(ServicePartitionKind.Singleton))
+                {
+                    await MapSingletonPartition(partitionsInPrimary, partitionsInSecondary, myDictionary);
+                }
+            }
+        }
+
+        public async Task MapInt64Partitions(System.Fabric.Query.ServicePartitionList primaryPartitions, System.Fabric.Query.ServicePartitionList secondaryPartitions, IReliableDictionary<Guid, PartitionWrapper> myDictionary)
+        {
+            foreach (var primaryPartition in primaryPartitions)
+            {
+                PartitionWrapper partitionWrapper = new PartitionWrapper(primaryPartition);
+                var int64PartitionInfo = primaryPartition.PartitionInformation as Int64RangePartitionInformation;
+                long? lowKeyPrimary = int64PartitionInfo?.LowKey;
+                foreach(var secondaryPartition in secondaryPartitions)
+                {
+                    long? lowKeySecondary = (secondaryPartition.PartitionInformation as Int64RangePartitionInformation)?.LowKey;
+                    if(lowKeyPrimary == lowKeySecondary)
+                    {
+                        using (var tx = this.StateManager.CreateTransaction())
+                        {
+                            var result = await myDictionary.TryAddAsync(tx, primaryPartition.PartitionInformation.Id, new PartitionWrapper(secondaryPartition));
+
+                            ServiceEventSource.Current.ServiceMessage(this.Context, result ? "Successfully Mapped Partition-{0} to Partition-{1}" : "Already Exists",primaryPartition.PartitionInformation.Id,secondaryPartition.PartitionInformation.Id);
+                            // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                            // discarded, and nothing is saved to the secondary replicas.
+                            await tx.CommitAsync();
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task MapNamedPartitions(System.Fabric.Query.ServicePartitionList primaryPartitions, System.Fabric.Query.ServicePartitionList secondaryPartitions, IReliableDictionary<Guid, PartitionWrapper> myDictionary)
+        {
+            foreach(var primaryPartition in primaryPartitions)
+            {
+                PartitionWrapper primaryPartitionWrapper = new PartitionWrapper(primaryPartition);
+                var partitionNamePrimary = (primaryPartition.PartitionInformation as NamedPartitionInformation).Name;
+                foreach (var secondaryPartition in secondaryPartitions)
+                {
+                    string partitionNameSecondary = (secondaryPartition.PartitionInformation as NamedPartitionInformation).Name;
+                    if (partitionNamePrimary == partitionNameSecondary)
+                    {
+                        using (var tx = this.StateManager.CreateTransaction())
+                        {
+                            var result = await myDictionary.TryAddAsync(tx, primaryPartition.PartitionInformation.Id, new PartitionWrapper(secondaryPartition));
+
+                            ServiceEventSource.Current.ServiceMessage(this.Context, result ? "Successfully Mapped Partition-{0} to Partition-{1}" : "Already Exists", primaryPartition.PartitionInformation.Id, secondaryPartition.PartitionInformation.Id);
+                            // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                            // discarded, and nothing is saved to the secondary replicas.
+                            await tx.CommitAsync();
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task MapSingletonPartition(System.Fabric.Query.ServicePartitionList primaryPartitions, System.Fabric.Query.ServicePartitionList secondaryPartitions, IReliableDictionary<Guid, PartitionWrapper> myDictionary)
+        {
+            using (var tx = this.StateManager.CreateTransaction())
+            {
+                var result = await myDictionary.TryAddAsync(tx, primaryPartitions[0].PartitionInformation.Id, new PartitionWrapper(secondaryPartitions[0]));
+
+                ServiceEventSource.Current.ServiceMessage(this.Context, result ? "Successfully Mapped Partition-{0} to Partition-{1}" : "Already Exists", primaryPartitions[0].PartitionInformation.Id, secondaryPartitions[0].PartitionInformation.Id);
+                // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                // discarded, and nothing is saved to the secondary replicas.
+                await tx.CommitAsync();
+            }
         }
     }
 }
